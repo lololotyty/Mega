f70ccf895cfaa16091c3fad0df3631dd6dc1e76f
wddwwdw <hangerskype@gmail.com>
2025-04-21 02:04:50 +0530

    Update _init_.py

--- megadl/modules/_init_.py
+++ megadl/modules/_init_.py
@@ -1,13 +1,3 @@
 # Module loader for megadl
-print("> Loading modules...")
-# Import all module files for Pyrogram to discover them
-from . import (
-    admin,
-    auth,
-    bonus,
-    callbacks,
-    generals,
-    mega_dl,
-    mega_up
-)
-print("> Modules loaded successfully!") 
+print("> Loading modules directory...")
+# Don't import modules here to avoid circular imports 
